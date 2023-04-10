19798be5c1392b28440f5286def773ac4048183e
Nguyen Phuoc Dac Hung <hung.nguyendachungbk@hcmut.edu.vn>
2023-04-10 17:55:41 +0700

    Hung first commit

--- admin/View/admin_view_detail.php
+++ admin/View/admin_view_detail.php
@@ -10,14 +10,16 @@ $detail_product = mysqli_fetch_array($get_product);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public/css/admin.css">
     <title><?php echo $detail_product['name'];?></title>
 </head>
 <body>
     <div class="info-book">
-        <img src="<?php echo $detail_product['image'];?>" alt="">
-        <div>
-            <h1><?php echo $detail_product['name'];?></h1>
-            <p><?php echo $detail_product['author'];?></p>
+        <img src="<?php echo $detail_product['image'];?>" alt="" class="info-book-img">
+        <div class="info-book-detail">
+            <h1 class="info-book-detail-name"></h1><?php echo $detail_product['name'];?></h1>
+            <p class="info-book-detail-author"></p><?php echo $detail_product['author'];?></p>
+            <p class="info-book-detail-price"></p><?php echo $detail_product['price'];?></p>
         </div>
     </div>
     
