acc7bb7af105e2d764e2342ddad873c740bf69a1
Thanh-Cong0602 <cong.nguyen0602@hcmut.edu.vn>
2023-04-24 18:07:42 +0700

    Thu 4 Demo

--- app/View/products_combo.php
+++ app/View/products_combo.php
@@ -11,6 +11,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Complete Book Store Online Website</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <!-- font awesome cdn link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
@@ -49,8 +50,27 @@ session_start();
     <section class="product" id="product" data-aos="fade-up" data-aos-delay="500">
         <div class="box-container ">
             <?php
-            $select_combo_products = mysqli_query($conn, "SELECT * FROM `combo_products` ORDER BY `date` ASC") or die('query failed');
-            if (mysqli_num_rows($select_combo_products) > 0) {
+             $per_page = 8;
+             $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+             $start = ($page - 1) * $per_page;
+             $select_combo_products = mysqli_query($conn, "SELECT * FROM `combo_products` ORDER BY `date` ASC") or die('query failed');
+             if (mysqli_num_rows($select_combo_products) > 0) {
+                while ($fetch_combo_products = mysqli_fetch_assoc($select_combo_products)) {
+            ?>
+
+            <?php 
+            $total_products = mysqli_query($conn, "SELECT COUNT(*) AS total FROM `combo_products`") or die('query failed');
+            $total_products = mysqli_fetch_assoc($total_products)['total'];
+            $total_pages = ceil($total_products / $per_page);
+            // Xác định trang hiện tại
+            $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+            // Tạo URL cho các trang
+            $url = "http://localhost:3000/app/View/shop.php?page=";
+            // Tính toán giới hạn của LIMIT trong câu truy vấn SQL
+                $offset = ($current_page - 1) * $per_page;
+              // Truy vấn sản phẩm trong cơ sở dữ liệu với LIMIT và OFFSET
+              $select_combo_products = mysqli_query($conn, "SELECT * FROM combo_products ORDER BY date ASC LIMIT $per_page OFFSET $offset") or die('query failed');    
+             if (mysqli_num_rows($select_combo_products) > 0) {
                 while ($fetch_combo_products = mysqli_fetch_assoc($select_combo_products)) {
             ?>
                     <form method="post" action="../Controllers/cartController.php">
@@ -78,7 +98,28 @@ session_start();
 
             <?php
                 }
-            } else {
+            }
+            ?>
+            <nav aria-label="Page navigation example" class="toolbar toolbar_combo">
+            <ul class="pagination justify-content-center d-flex flex-wrap">
+                <li class="page-item <?php echo ($current_page <= 1) ? 'disabled' : ''; ?>">
+                <a class="page-link" href="<?php echo $url . ($current_page - 1); ?>" tabindex="-1">Previous</a>
+                </li>
+                <?php for ($i = 1; $i <= $total_pages; $i++) : ?>
+                <li class="page-item <?php echo ($i == $current_page) ? 'active' : ''; ?>">
+                <a class="page-link" href="<?php echo $url . $i; ?>"><?php echo $i; ?></a>
+                </li>
+                <?php endfor; ?>
+                <li class="page-item <?php echo ($current_page >= $total_pages) ? 'disabled' : ''; ?>">
+                <a class="page-link" href="<?php echo $url . ($current_page + 1); ?>">Next</a>
+                </li>
+            </ul>
+            </nav>
+            ?>
+            <?php
+            }
+        }
+            else {
                 echo '<p class="empty">no products added yet!</p>';
             }
             ?>
--- app/View/shop.php
+++ app/View/shop.php
@@ -11,7 +11,8 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Complete Book Store Online Website</title>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
     <!-- font awesome cdn link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
@@ -44,41 +45,85 @@ session_start();
     </div>
     <!-- banner section ends -->
 
-    <!-- products section starts  -->
+
+
 
     <section class="product" id="product" data-aos="fade-up" data-aos-delay="500">
         <div class="box-container shop">
             <?php
+            $per_page = 8;
+            $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+            $start = ($page - 1) * $per_page;
+
             $select_products = mysqli_query($conn, "SELECT * FROM `products` ORDER BY `date` ASC") or die('query failed');
             if (mysqli_num_rows($select_products) > 0) {
                 while ($fetch_products = mysqli_fetch_assoc($select_products)) {
             ?>
-            <form method="post" action="../Controllers/cartController.php">
-                <div class="box" data-aos="fade-up" data-aos-delay="300">
-                    <div class="image">
-                        <img src="<?php echo $fetch_products['image']; ?>" alt="">
-                    </div>
-                    <div class="content">
-                        <h3><?php echo $fetch_products['name']; ?></h3>
-                        <a href="detail_book.php?get_id=<?php echo $fetch_products['product_id']; ?>">Xem thêm<i
-                                class="fas fa-angle-right"></i></a>
-                    </div>
-                    <div class="purchase">
-                        <h3><?php echo $fetch_products['price']; ?>
-                            <span class="rate">₫</span>
-                        </h3>
-                        </h3>
-                        <input type="hidden" name="product_quantity" value="1">
-                        <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
-                        <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
-                        <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-                        <button type="submit" name="add_to_cart">
-                            <i class="fas fa-shopping-cart"></i>
-                        </button>
-                    </div>
-                </div>
-            </form>
+            
+            <?php 
+            $total_products = mysqli_query($conn, "SELECT COUNT(*) AS total FROM `products`") or die('query failed');
+            $total_products = mysqli_fetch_assoc($total_products)['total'];
+            $total_pages = ceil($total_products / $per_page);
+            // Xác định trang hiện tại
+                $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
 
+                // Tạo URL cho các trang
+                $url = "http://localhost:3000/app/View/shop.php?page=";
+
+                // Tính toán giới hạn của LIMIT trong câu truy vấn SQL
+                $offset = ($current_page - 1) * $per_page;
+
+                // Truy vấn sản phẩm trong cơ sở dữ liệu với LIMIT và OFFSET
+                $select_products = mysqli_query($conn, "SELECT * FROM products ORDER BY date ASC LIMIT $per_page OFFSET $offset") or die('query failed');
+                if (mysqli_num_rows($select_products) > 0) {
+                while ($fetch_products = mysqli_fetch_assoc($select_products)) {
+                ?>
+                <form method="post" action="../Controllers/cartController.php">
+                <div class="box" data-aos="fade-up" data-aos-delay="300">
+                <div class="image">
+                <img src="<?php echo $fetch_products['image']; ?>" alt="">
+                </div>
+                <div class="content">
+                <h3><?php echo $fetch_products['name']; ?></h3>
+                <a href="detail_book.php?get_id=<?php echo $fetch_products['product_id']; ?>">Xem thêm<i
+                                    class="fas fa-angle-right"></i></a>
+                </div>
+                <div class="purchase">
+                <h3><?php echo $fetch_products['price']; ?>
+                <span class="rate">₫</span>
+                </h3>
+                <input type="hidden" name="product_quantity" value="1">
+                <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+                <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+                <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+                <button type="submit" name="add_to_cart">
+                <i class="fas fa-shopping-cart"></i>
+                </button>
+                </div>
+                </div>
+                </form>
+
+                <?php
+                    }
+                }
+// Tạo thanh        pagination
+                ?>
+               <nav aria-label="Page navigation example" class="toolbar">
+            <ul class="pagination justify-content-center d-flex flex-wrap">
+                <li class="page-item <?php echo ($current_page <= 1) ? 'disabled' : ''; ?>">
+                <a class="page-link" href="<?php echo $url . ($current_page - 1); ?>" tabindex="-1">Previous</a>
+                </li>
+                <?php for ($i = 1; $i <= $total_pages; $i++) : ?>
+                <li class="page-item <?php echo ($i == $current_page) ? 'active' : ''; ?>">
+                <a class="page-link" href="<?php echo $url . $i; ?>"><?php echo $i; ?></a>
+                </li>
+                <?php endfor; ?>
+                <li class="page-item <?php echo ($current_page >= $total_pages) ? 'disabled' : ''; ?>">
+                <a class="page-link" href="<?php echo $url . ($current_page + 1); ?>">Next</a>
+                </li>
+            </ul>
+            </nav>
+            ?>
             <?php
                 }
             } else {
@@ -86,7 +131,6 @@ session_start();
             }
             ?>
         </div>
-
 
     </section>
 
